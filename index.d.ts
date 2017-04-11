2fb657be85cd4cc6ded099fb40583f509b1fff50
Sergej Filatov <sergej.filatov@ideaportriga.lv>
2017-04-11 14:59:51 +0300

    fixes

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,33 @@
-export {};
+export class EventId {
+    time: Date;
+    seq: number;
+
+    constructor(t: Date, seq: number) {
+        this.time = t;
+        this.seq = seq;
+    }
+}
+
+export class Event {
+    _id: EventId;
+    id: string;
+    timestamp: string;
+    payload: any;
+    headers: { [key:string]:string; };
+    metrics: { [key:string]:number };
+
+    constructor(id:EventId) {
+        this._id = id;
+    }
+}
+
+export class DM {
+    init(endpoint:string, authkey:string): void ;
+    send(data:Event): void ;
+}
 
 declare global {
     interface Window {
-        dm: any;
+        dm: DM;
     }
 }
